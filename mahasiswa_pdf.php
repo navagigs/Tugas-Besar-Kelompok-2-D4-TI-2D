ffc9bfd130d0e542f6bc8ce995ff531b67fbaddf
bayurizki135 <bayurizki135@yahoo.com>
2016-05-25 21:22:49 +0700

    update

--- mahasiswa_pdf.php
+++ mahasiswa_pdf.php
@@ -16,3 +16,32 @@
         </style>
     </head>
     <body>
+        <h2>Mahasiswa List</h2>
+        <table class="word-table" style="margin-bottom: 10px">
+            <tr>
+                <th>No</th>
+                <th>Mahasiswa Npm</th>
+                <th>Mahasiswa Nama</th>
+                <th>Mahasiswa Alamat</th>
+                <th>Mahasiswa Email</th>
+                <th>Mahasiswa Tlp</th>
+                <th>Mahasiswa Agama</th>
+                <th>Kelas Id</th>
+            </tr>
+            <?php foreach ($mahasiswa_data as $mahasiswa) { ?>
+                <tr>
+                  <td><?php echo ++$start ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_npm ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_nama ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_alamat ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_email ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_tlp ?></td>
+                  <td><?php echo $mahasiswa->mahasiswa_agama ?></td>
+                  <td><?php echo $mahasiswa->kelas_id ?></td>   
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+    </body>
+</html>
