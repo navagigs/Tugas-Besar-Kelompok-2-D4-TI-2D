0c2ecfd39dc7700b1895a5c6473c7be8e5e8a845
Alfi Zulfian Abdi <Alfi Zulfian Abdi>
2016-05-25 17:25:06 +0700

    insert

---
+++ backend_ws/application/views/kelas_form.php
@@ -0,0 +1,14 @@
+<!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                
+                  <h3 class='box-title'>KELAS</h3>
+                      <div class='box box-primary'>
+    </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
