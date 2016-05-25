2eeaaadc4f23f18e3c2110d5af53f813a644af73
bayurizki135 <bayurizki135@yahoo.com>
2016-05-25 21:07:49 +0700

    insert

---
+++ mahasiswa_read.php
@@ -0,0 +1,22 @@
+
+        <!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                <h3 class='box-title'>Mahasiswa Read</h3>
+        <table class="table table-bordered">
+	    <tr><td>Mahasiswa Npm</td><td><?php echo $mahasiswa_npm; ?></td></tr>
+	    <tr><td>Mahasiswa Nama</td><td><?php echo $mahasiswa_nama; ?></td></tr>
+	    <tr><td>Mahasiswa Alamat</td><td><?php echo $mahasiswa_alamat; ?></td></tr>
+	    <tr><td>Mahasiswa Email</td><td><?php echo $mahasiswa_email; ?></td></tr>
+	    <tr><td>Mahasiswa Tlp</td><td><?php echo $mahasiswa_tlp; ?></td></tr>
+	    <tr><td>Mahasiswa Agama</td><td><?php echo $mahasiswa_agama; ?></td></tr>
+	    <tr><td></td><td><a href="<?php echo site_url('mahasiswa') ?>" class="btn btn-default">Cancel</a></td></tr>
+	</table>
+        </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
