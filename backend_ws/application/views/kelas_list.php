cab0bd1545c2b1c88c95f180bc1b2367f9b9f4af
fakbarrr <faisalakbarrrrr@gmail.com>
2016-05-25 17:48:38 +0700

    insert

---
+++ backend_ws/application/views/kelas_list.php
@@ -0,0 +1,26 @@
+
+        <!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                  <h3 class='box-title'>KELAS LIST <?php echo anchor('kelas/create/','Create',array('class'=>'btn btn-danger btn-sm'));?>
+		<?php echo anchor(site_url('kelas/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
+		<?php echo anchor(site_url('kelas/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
+		<?php echo anchor(site_url('kelas/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?></h3>
+                </div><!-- /.box-header -->
+                
+        <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+        <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("#mytable").dataTable();
+            });
+        </script>
+                    </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
