835b9880b8728e07cd2ac5cb162e4e884e37b8ab
ayuanggara24 <ayuanggaraspentwo@gmail.com>
2016-05-25 16:49:20 +0700

    update dosenpdf

--- backend_ws/application/views/dosen_pdf.php
+++ backend_ws/application/views/dosen_pdf.php
@@ -15,4 +15,28 @@
             }
         </style>
     </head>
+    <body>
+        <h2>Dosen List</h2>
+        <table class="word-table" style="margin-bottom: 10px">
+            <tr>
+                <th>No</th>
+        <th>Dosen Nik</th>
+        <th>Dosen Nama</th>
+        <th>Dosen Matkul</th>
+        
+            </tr><?php
+            foreach ($dosen_data as $dosen)
+            {
+                ?>
+                <tr>
+              <td><?php echo ++$start ?></td>
+              <td><?php echo $dosen->dosen_nik ?></td>
+              <td><?php echo $dosen->dosen_nama ?></td>
+              <td><?php echo $dosen->dosen_matkul ?></td>   
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+    </body>
 </html>
